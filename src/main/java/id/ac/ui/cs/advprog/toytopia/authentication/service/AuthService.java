69240a7014a3d071fcc832a9ab3fe00469ec112c
Bimantoro <bimantoro.widyadana@ui.ac.id>
2024-05-24 01:34:53 +0700

    make auth service

--- src/main/java/id/ac/ui/cs/advprog/toytopia/authentication/service/AuthService.java
+++ src/main/java/id/ac/ui/cs/advprog/toytopia/authentication/service/AuthService.java
@@ -1,2 +1,10 @@
-package id.ac.ui.cs.advprog.toytopia.authentication.service;public class AuthService {
+package id.ac.ui.cs.advprog.toytopia.authentication.service;
+
+import id.ac.ui.cs.advprog.toytopia.authentication.model.dto.AuthenticationRequest;
+import id.ac.ui.cs.advprog.toytopia.authentication.model.dto.AuthenticationResponse;
+import id.ac.ui.cs.advprog.toytopia.authentication.model.dto.RegisterRequest;
+
+public interface AuthService {
+    AuthenticationResponse register(RegisterRequest request);
+    AuthenticationResponse authenticate(AuthenticationRequest request);
 }
--- src/main/java/id/ac/ui/cs/advprog/toytopia/authentication/service/AuthServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/toytopia/authentication/service/AuthServiceImpl.java
@@ -1,2 +1,64 @@
-package id.ac.ui.cs.advprog.toytopia.authentication.service;public class AuthServiceImpl {
+package id.ac.ui.cs.advprog.toytopia.authentication.service;
+
+import id.ac.ui.cs.advprog.toytopia.authentication.model.Role;
+import id.ac.ui.cs.advprog.toytopia.authentication.model.User;
+import id.ac.ui.cs.advprog.toytopia.authentication.model.dto.AuthenticationRequest;
+import id.ac.ui.cs.advprog.toytopia.authentication.model.dto.AuthenticationResponse;
+import id.ac.ui.cs.advprog.toytopia.authentication.model.dto.RegisterRequest;
+import id.ac.ui.cs.advprog.toytopia.authentication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AuthServiceImpl(UserRepository repository,
+                           PasswordEncoder passwordEncoder,
+                           JwtService jwtService,
+                           AuthenticationManager authenticationManager) {
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public AuthenticationResponse register(RegisterRequest request) {
+        User user = User
+                .builder()
+                .email(request.getEmail())
+                .username(request.getUsername())
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .address(request.getAddress())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        String jwtToken = jwtService.generateToken(user);
+        return new AuthenticationResponse(jwtToken);
+    }
+
+    @Override
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        // An exception will be thrown if username or password isn't correct
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+        User user = repository.findByUsername(request.getUsername())
+                .orElseThrow();
+        String jwtToken = jwtService.generateToken(user);
+        return new AuthenticationResponse(jwtToken);
+    }
 }
