6fd0b263a8f7ac229688461b7246a217f8f32bc1
Bimantoro <bimantoro.widyadana@ui.ac.id>
2024-05-24 00:34:53 +0700

    add jwt secret and change app properties to user env vars

--- src/main/java/id/ac/ui/cs/advprog/toytopia/authentication/service/JwtService.java
+++ src/main/java/id/ac/ui/cs/advprog/toytopia/authentication/service/JwtService.java
@@ -1,2 +1,7 @@
-package id.ac.ui.cs.advprog.toytopia.authentication.service;public class JwtService {
+package id.ac.ui.cs.advprog.toytopia.authentication.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
 }
