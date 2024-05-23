6a5d69a7e669f36fac895efdebcdcc607ba01b55
Bimantoro <bimantoro.widyadana@ui.ac.id>
2024-05-24 01:15:42 +0700

    create security config

--- src/main/java/id/ac/ui/cs/advprog/toytopia/authentication/config/SecurityConfiguration.java
+++ src/main/java/id/ac/ui/cs/advprog/toytopia/authentication/config/SecurityConfiguration.java
@@ -1,2 +1,43 @@
-package id.ac.ui.cs.advprog.toytopia.authentication.config;public class SecurityConfiguration {
+package id.ac.ui.cs.advprog.toytopia.authentication.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthenticationFilter jwtFilter;
+
+    @Autowired
+    public SecurityConfiguration(AuthenticationProvider authenticationProvider, JwtAuthenticationFilter jwtFilter) {
+        this.authenticationProvider = authenticationProvider;
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorizeHttpRequest) -> {
+                    authorizeHttpRequest
+                            .requestMatchers("api/auth/**").permitAll()
+                            .anyRequest().authenticated();
+                })
+                .sessionManagement((sessionManagement) -> {
+                    sessionManagement
+                            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
 }
