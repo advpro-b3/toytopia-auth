c8f5046ddd43397fd55aadd62d9b49772e0b5460
Bimantoro <bimantoro.widyadana@ui.ac.id>
2024-05-24 00:52:40 +0700

    create a user details service implementation

---
+++ src/main/java/id/ac/ui/cs/advprog/toytopia/authentication/service/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package id.ac.ui.cs.advprog.toytopia.authentication.service;
+
+import id.ac.ui.cs.advprog.toytopia.authentication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        String.format("User with username %s not found.", username))
+                );
+    }
+}
