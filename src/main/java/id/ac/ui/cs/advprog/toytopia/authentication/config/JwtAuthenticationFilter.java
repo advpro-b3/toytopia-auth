0317e75a541af24a6053fe6efd1338e5a175e9ce
Bimantoro <bimantoro.widyadana@ui.ac.id>
2024-05-24 01:00:18 +0700

    finish jwt auth filter

--- src/main/java/id/ac/ui/cs/advprog/toytopia/authentication/config/JwtAuthenticationFilter.java
+++ src/main/java/id/ac/ui/cs/advprog/toytopia/authentication/config/JwtAuthenticationFilter.java
@@ -1,10 +1,19 @@
 package id.ac.ui.cs.advprog.toytopia.authentication.config;
 
+import id.ac.ui.cs.advprog.toytopia.authentication.service.JwtService;
+import id.ac.ui.cs.advprog.toytopia.authentication.service.JwtServiceImpl;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,6 +21,15 @@ import java.io.IOException;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -23,6 +41,24 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
             return;
         }
+
         final String jwtToken = authHeader.substring(7);
+        final String username = jwtService.extractUsername(jwtToken);
+        if (username != null || SecurityContextHolder.getContext().getAuthentication() == null) {
+            // Check if user with the specified username exists in the database. If not, throw an exception
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if (jwtService.isTokenValid(jwtToken, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 }
